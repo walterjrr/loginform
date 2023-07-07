4eb1e39842c6c47feb999831fcb4eaaee11c0e2f
walterjrr <jrwalter731@gmail.com>
2023-07-07 13:13:49 -0300

    verification of acess with password and login

--- index.js
+++ index.js
@@ -1,7 +1,6 @@
 // Importando os módulos necessários
 const express = require('express');
 const bodyParser = require('body-parser');
-
 const fs = require('fs');
 
 // Criando uma instância do aplicativo Express
@@ -15,14 +14,15 @@ app.use(bodyParser.urlencoded({ extended: true }));
 
 // Definindo uma rota básica
 app.get('/', (req, res) => {
-    res.sendFile(__dirname + '/index.html');
+  res.sendFile(__dirname + '/index.html');
 });
 
 app.get('/cadastrado', (req, res) => {
-    const email = req.query.email;
-    const password = req.query.password;
+  const email = req.query.email;
+  const password = req.query.password;
+  const userName = req.query.userName;
 
-    const filePath = __dirname + '/public/cadastrado.html';
+  const filePath = __dirname + '/public/cadastrado.html';
 
   fs.readFile(filePath, 'utf8', (err, data) => {
     if (err) {
@@ -31,21 +31,61 @@ app.get('/cadastrado', (req, res) => {
     }
 
     // Substituir os placeholders no HTML pelos valores do email e senha
-    const updatedData = data.replace('{{email}}', email).replace('{{password}}', password);
+    const updatedData = data.replace('{{email}}', email).replace('{{password}}', password).replace('{{userName}}', userName);
     res.send(updatedData);
   });
 });
 
-
 app.post('/enviar-formulario', (req, res) => {
-    email = req.body.email
-    password = req.body.password
-    console.log(email, password)
+  const email = req.body.email;
+  const password = req.body.password;
+  const userName = req.body.userName;
+  console.log(email, password, userName);
 
-    res.redirect(`/cadastrado?email=${email}&password=${password}`)
-})
+  res.redirect(`/cadastrado?email=${email}&password=${password}&userName=${userName}`);
+});
 
-// Definindo outras rotas e lógica de negócio
+app.get('/logado', (req, res) => {
+  const login = req.query.login;
+  const senha = req.query.senha;
+  const userName = req.query.userName
+
+  const filePath = __dirname + '/public/logado.html';
+
+  fs.readFile(filePath, 'utf8', (err, data) => {
+    if (err) {
+      res.status(500).send('Erro ao ler o arquivo');
+      return;
+    }
+
+    // Substituir os placeholders no HTML pelos valores do email e senha
+    const updatedData = data.replace('{{login}}', login).replace('{{senha}}', senha).replace('{{userName}}', userName);
+    res.send(updatedData);
+  });
+});
+
+app.post('/enviar-formulario2', (req, res) => {
+  const email = req.body.email;
+  const password = req.body.password;
+  const userName = req.body.userName;
+  const login = req.body.login;
+  const senha = req.body.senha;
+
+  console.log(login, senha, email, password, userName);
+
+  if (email === login && password === senha) {
+    console.log('Você está logado', email, password, userName);
+    res.redirect(`/logado?userName=${userName}`);
+  } else {
+    console.log('Erro de login');
+    res.redirect('/login-error');
+  }
+});
+
+// Rota para exibir uma página de erro de login
+app.get('/login-error', (req, res) => {
+  res.send('Erro de login. Por favor, verifique suas credenciais e tente novamente.');
+});
 
 // Iniciando o servidor
 const port = 3000;
