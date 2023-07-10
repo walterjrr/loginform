545489a1698741bd73c53e6538715a13d223a124
walterjrr <jrwalter731@gmail.com>
2023-07-10 12:38:18 -0300

    image render with multer

--- index.js
+++ index.js
@@ -1,16 +1,33 @@
 // Importando os módulos necessários
 const express = require('express');
 const bodyParser = require('body-parser');
+const multer = require('multer');
 const fs = require('fs');
+const path = require('path');
 
 // Criando uma instância do aplicativo Express
 const app = express();
 
-app.use(express.static('public'));
+app.use('/public', express.static(path.join(__dirname, 'public')));
 
 // Configurando o middleware para analisar o corpo das requisições
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
+
+// Configuração do Multer
+const storage = multer.diskStorage({
+  destination: function (req, file, cb) {
+    const destino = path.join(__dirname, 'public', 'imagens');
+    cb(null, 'public/imagens/');
+  },
+  filename: function (req, file, cb) {
+    const uniqueSuffix = Date.now() + '-' + Math.round(Math.random() * 1e9);
+    cb(null, file.fieldname + '-' + uniqueSuffix + '.' + file.originalname.split('.').pop());
+  }
+});
+
+const upload = multer({ storage: storage });
+
 
 // Definindo uma rota básica
 app.get('/', (req, res) => {
@@ -49,6 +66,7 @@ app.get('/logado', (req, res) => {
   const login = req.query.login;
   const senha = req.query.senha;
   const userName = req.query.userName
+  const caminhoImagem = req.query.imagem
 
   const filePath = __dirname + '/public/logado.html';
 
@@ -59,7 +77,11 @@ app.get('/logado', (req, res) => {
     }
 
     // Substituir os placeholders no HTML pelos valores do email e senha
-    const updatedData = data.replace('{{login}}', login).replace('{{senha}}', senha).replace('{{userName}}', userName);
+    const updatedData = data.replace('{{login}}', login)
+      .replace('{{senha}}', senha)
+      .replace('{{userName}}', userName)
+      .replace('{{caminhoImagem}}', caminhoImagem);
+
     res.send(updatedData);
   });
 });
@@ -81,6 +103,42 @@ app.post('/enviar-formulario2', (req, res) => {
     res.redirect('/login-error');
   }
 });
+
+app.post('/upload-imagem', upload.single('imagem'), (req, res) => {
+  try {
+    // A imagem foi carregada com sucesso
+    const imagem = req.file;
+
+    // Verificar se uma imagem foi enviada
+    if (!imagem) {
+      res.status(400).send('Nenhuma imagem foi enviada.');
+      return;
+    }
+
+    // Salvar a imagem em um diretório específico
+    const caminhoImagem = 'public/imagens/' + imagem.filename;
+    fs.rename(imagem.path, caminhoImagem, (err) => {
+      if (err) {
+        console.error('Erro ao salvar a imagem:', err);
+        res.status(500).send('Erro ao salvar a imagem.');
+        return;
+      }
+
+      // Realizar as ações desejadas com o caminho da imagem, como salvá-lo em uma constante
+      console.log('Caminho da imagem:', caminhoImagem);
+      console.log(req.body.userName)
+
+      // Redirecionar para a página logado.html com o caminho da imagem
+      res.redirect(`/logado?imagem=${encodeURIComponent(caminhoImagem)}&userName=${encodeURIComponent(req.body.userName)}`);
+    });
+  } catch (err) {
+    console.error('Erro ao processar a solicitação:', err);
+    res.status(500).send('Erro ao processar a solicitação.');
+  }
+});
+
+
+
 
 // Rota para exibir uma página de erro de login
 app.get('/login-error', (req, res) => {
